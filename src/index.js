daf61433ee2f8c544ecd410807abbe18ada777ba
MCH <111715559+mchxyz@users.noreply.github.com>
2025-02-25 23:44:49 -0500

    Added prompt improvement

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function generatePoem(event) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "aob6b1bc95bbcf3taf0ee93f974adfb4";
     let prompt = `Generate a French poem about ${instructionsInput.value}`;
-    let context = "User instructions: You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Please, make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and not at the beginning.";
+    let context = "User instructions: You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Please, make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and not at the beginning. Also, please remove the HTML abbreviation from the poem and any backticks found in the poem as well.";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     console.log("Generating poem");

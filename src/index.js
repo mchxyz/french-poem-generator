406d8458b6d85644685110e078e342de0d821a49
MCH <111715559+mchxyz@users.noreply.github.com>
2025-02-26 13:11:08 -0500

    Fixed blink effect

--- src/index.js
+++ src/index.js
@@ -12,21 +12,17 @@ function displayPoem(response) {
 function generatePoem(event) {
     event.preventDefault();
 
-    // 1. Build the API URL
-
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "aob6b1bc95bbcf3taf0ee93f974adfb4";
-    let prompt = `Generate a French poem about ${instructionsInput.value}`;
     let context = "User instructions: You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Please, make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and not at the beginning. Also, please remove the HTML abbreviation from the poem and any backticks found in the poem as well.";
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    let prompt = `Generate a French poem about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let poemElement = document.querySelector("#poem");
     poemElement.classList.remove("hidden");
     poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
 
-    // 2. Make a call to the API
-
-    axios.get(apiUrl).then(displayPoem);
+    axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
